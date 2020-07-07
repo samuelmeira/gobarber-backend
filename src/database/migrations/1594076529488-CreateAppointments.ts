b164b772a0aece1d7dd1b04f4a466c6b18b9f531
Samuel Meira <samuelleandmeira@hotmail.com>
2020-07-07 19:51:03 -0300

    add appointment and user relation

--- src/database/migrations/1594076529488-CreateAppointments.ts
+++ src/database/migrations/1594076529488-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1594076529488 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
